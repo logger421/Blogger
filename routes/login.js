6db9088faa75729b21087c1bbc17fbd6620c40d9
logger421 <wojteklotko3@gmail.com>
2023-02-13 23:54:35 +0100

    Added login

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+/*TODO Upgrade login system to database persistent*/
+let users = [];
+
 router.get('/', function(req, res, next) {
-    res.render('login');
+    res.render('login', {title: 'Login'});
 });
 
+router.post('/', async (req, res, next) => {
+    user = {
+        login: req.body.username,
+        password: req.body.password
+    }
+    users.push(user);
+    console.log(users);
+    res.redirect('/home');
+});
 module.exports = router;
