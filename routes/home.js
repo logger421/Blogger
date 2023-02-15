4870dcfc6250d06f97c91f084e3f0b5ed6362c62
logger421 <wojteklotko3@gmail.com>
2023-02-15 23:08:55 +0100

    Updated home page added packages.

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+let blogs = [{id: 1, title: "Blog1", content: "Lorem ipsum1"}, {id: 2, title: "Blog2", content: "Lorem ipsum2"}, {id: 3, title: "Blog3", content: "Lorem ipsum3"}]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+  res.render('home', { title: 'Home', blogs: blogs});
 });
 
 module.exports = router;
