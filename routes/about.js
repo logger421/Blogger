0001b47aedbd243e5df8d5d83cde594a9a08f396
logger421 <wojteklotko3@gmail.com>
2023-02-19 18:27:08 +0100

    Added about-us. Updated background images.

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('about-us');
+    res.render('about-us', {title: 'About'});
 });
 
 module.exports = router;
